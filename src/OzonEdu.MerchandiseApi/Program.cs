2316217076bc236a206dcc5e3f2cf187c3ae1736
Евгений Рахимов <Alterman69@mail.ru>
2021-10-28 21:31:00 +0700

    Renamed solution, project and main namespace.

--- src/OzonEdu.MerchandiseApi/Program.cs
+++ src/OzonEdu.MerchandiseApi/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace OzonEdu.MerchandiseService
+namespace OzonEdu.MerchandiseApi
 {
     public class Program
     {
